e6d11f623b8f8a40b0c81a0174ac0244df1589fd
Zinchenko <zinchenko.vlad02@gmail.com>
2022-10-04 15:23:24 +0300

    added fizzbuzz

---
+++ easy_level/412_Fizz_Buzz.py
@@ -0,0 +1,23 @@
+def fizzBuzz(n):
+    
+    list = []
+
+    for i in range(1, n+1):
+        if  i % 3 == 0 and i % 5 == 0:
+            list.append("FizzBuzz")
+        elif  i % 3 == 0:
+                list.append("Fizz")
+        elif  i % 5 == 0:
+                list.append("Buzz")    
+        else:
+            list.append(str(i))
+
+    return list
+
+
+def main():
+    print(fizzBuzz(15))
+
+
+if __name__ == "__main__":
+    main()
